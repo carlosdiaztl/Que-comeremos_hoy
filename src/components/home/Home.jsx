8359a2ea58e3ff2d54dec40e83fd0578f53acb6e
Rafiky <rafikyop@gmail.com>
2022-12-02 21:44:08 -0500

    Proceso del main con dado moviendose

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,30 +1,35 @@
 import React from "react";
 import Navbar from "../restaurants/Navbar";
 import "./home.scss";
+
 const Home = () => {
   return (
     <>
       <Navbar />
       <div>
-        <h1>¿Qué comeremos hoy?</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-          similique perspiciatis eaque exercitationem. Ex eius aperiam corporis
-          provident nihil facere aut natus unde sapiente esse, et voluptates
-          nostrum culpa quos.
-        </p>
-        <div>
-          <div className="contenedor">
-            <div className={"dado"}>
-              <div className="lado uno"></div>
-              <div className="lado dos"></div>
-              <div className="lado tres"></div>
-              <div className="lado cuatro"></div>
-              <div className="lado cinco"></div>
-              <div className="lado seis"></div>
-            </div>
+        <hr />
+        <section className="home">
+          <div className="home__section1">
+            <h1 className="home__title">¿Qué comeremos hoy?</h1>
+            <p className="home__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatibus similique perspiciatis eaque exercitationem.
+            </p>
           </div>
-        </div>
+          <div>
+            <div className="contenedor">
+              <div className={"dado2"}>
+                <div className="lado uno"></div>
+                <div className="lado dos"></div>
+                <div className="lado tres"></div>
+                <div className="lado cuatro"></div>
+                <div className="lado cinco"></div>
+                <div className="lado seis"></div>
+              </div>
+            </div>
+            <button className="login__buttonSesion">Iniciar sesión</button>
+          </div>
+        </section>
       </div>
     </>
   );
